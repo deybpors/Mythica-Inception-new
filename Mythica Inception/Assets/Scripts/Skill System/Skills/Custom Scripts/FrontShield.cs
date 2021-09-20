2f0b01ae6a1f366973d879be250f5c61856eb5d2
deybpors <60822915+deybpors@users.noreply.github.com>
2021-09-21 07:22:08 +0800

    new changes

---
+++ Mythica Inception/Assets/Scripts/Skill System/Skills/Custom Scripts/FrontShield.cs
@@ -0,0 +1,22 @@
+﻿using _Core.Others;
+using Skill_System.Targeting_Type_Scripts;
+using UnityEngine;
+
+namespace Skill_System.Skills.Custom_Scripts
+{
+    [CreateAssetMenu(menuName = "Skill System/Skills/Sample Toggle Skill")]
+    public class FrontShield : ToggleTargetSkill
+    {
+        public override void Activate(IEntity entity)
+        {
+            //change stats here
+            Debug.Log("Activated " + skillName);
+        }
+
+        protected override void Deactivate(IEntity entity)
+        {
+            //change to default stats
+            Debug.Log("Deactivated " + skillName);
+        }
+    }
+}
---
+++ Mythica Inception/Assets/Scripts/_Core/Others/ProjectileRelease.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+namespace _Core.Others
+{
+    [System.Serializable]
+    public class ProjectileRelease
+    {
+        public Transform front;
+        public Transform back;
+        public Transform left;
+        public Transform right;
+    }
+}
